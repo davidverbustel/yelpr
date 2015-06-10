624086fbe967caa62dad18206ad29d3da14340de
David Verbustel <david.verbustel@gmail.com>
2015-06-10 11:09:14 +0200

    added dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20150610083233) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.string   "phone"
+    t.string   "phone_number"
     t.string   "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "reviews", force: :cascade do |t|
